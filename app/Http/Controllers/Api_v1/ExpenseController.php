3288a0fd45c666b79cd7136a651ce19b8c4393be
isam chajia <isamchajiabeat.com>
2025-03-19 11:23:01 +0000

    add gestion tag

--- app/Http/Controllers/Api_v1/ExpenseController.php
+++ app/Http/Controllers/Api_v1/ExpenseController.php
@@ -2,84 +2,52 @@
 
 namespace App\Http\Controllers\Api_v1;
 
-use App\Http\Controllers\Controller;
 use App\Models\Expense;
 use App\Models\Group;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ExpenseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function store(Request $request, Group $group)
+    {
+        $request->validate([
+            'amount' => 'required|numeric',
+            'description' => 'required|string',
+        ]);
+
+        $expense = Expense::create([
+            'amount' => $request->amount,
+            'description' => $request->description,
+            'user_id' => $request->user()->id,
+            'group_id' => $group->id,
+        ]);
+
+        return response()->json($expense, 201);
+    }
+
     public function index(Group $group)
     {
         $expenses = $group->expenses;
         return response()->json($expenses);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request, Group $group)
-    {
-        $request->validate([
-            'amount' => 'required|numeric',
-            'description' => 'required|string',
-            'payer_id' => 'required|exists:users,id',
-            'split_type' => 'required|in:equal,percentage',
-            'split_details' => 'required_if:split_type,percentage|array',
-        ]);
-
-        $expense = Expense::create([
-            'amount' => $request->amount,
-            'description' => $request->description,
-            'payer_id' => $request->payer_id,
-            'group_id' => $group->id,
-            'split_type' => $request->split_type,
-            'split_details' => $request->split_details,
-        ]);
-
-        return response()->json($expense, 201);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Expense $expense)
     {
-        return response()->json($expense->load('tags'));
-    }
-
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Expense $expense)
-    {
-        $expense->update($request->all());
-
         return response()->json($expense);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    public function update(Request $request, Expense $expense)
+    {
+        $request->validate([
+            'amount' => 'numeric',
+            'description' => 'string',
+        ]);
+
+        $expense->update($request->all());
+        return response()->json($expense);
+    }
+
     public function destroy(Expense $expense)
     {
         $expense->delete();
--- app/Http/Controllers/Api_v1/TagController.php
+++ app/Http/Controllers/Api_v1/TagController.php
@@ -2,69 +2,43 @@
 
 namespace App\Http\Controllers\Api_v1;
 
-use App\Http\Controllers\Controller;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TagController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return response()->json(Tag::all());
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $request->validate(['name' => 'required|unique:tags']);
+        $request->validate([
+            'name' => 'required|string',
+        ]);
 
-        $tag = Tag::create(['name' => $request->name]);
-
+        $tag = Tag::create($request->all());
         return response()->json($tag, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Tag $tag)
+    public function index()
     {
-        return response()->json($tag->load('expenses'));
+        $tags = Tag::all();
+        return response()->json($tags);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Tag $tag)
+    public function show(Tag $tag)
     {
-        $tag->update(['name' => $request->name]);
-
         return response()->json($tag);
     }
 
+    public function update(Request $request, Tag $tag)
+    {
+        $request->validate([
+            'name' => 'string',
+        ]);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+        $tag->update($request->all());
+        return response()->json($tag);
+    }
+
     public function destroy(Tag $tag)
     {
         $tag->delete();
